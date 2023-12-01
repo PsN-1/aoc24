f3d91f0bc24333bb69b1da9c234562980052ff65
Pedro Neto <pedro.severino@g.globo>
2023-12-01 09:52:22 -0300

    Initial Commit

---
+++ aoc24/aoc24.h
@@ -0,0 +1,18 @@
+//
+//  aoc24.h
+//  aoc24
+//
+//  Created by PEDRO SEVERINO DOS SANTOS NETO on 01/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for aoc24.
+FOUNDATION_EXPORT double aoc24VersionNumber;
+
+//! Project version string for aoc24.
+FOUNDATION_EXPORT const unsigned char aoc24VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aoc24/PublicHeader.h>
+
+
